8ac7dab00458beb724daba3a166a890d41c2b773
VuongMinhKhanh <2151050191khanh@ou.edu.vn>
2024-04-03 11:24:56 +0700

    Load Subjects to ComboBox

---
+++ OnlineTestingApp_DAO/Subject_DAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionBank_DAO
+{
+    public class Subject_DAO : DBConnect
+    {
+        public DataTable getSubjects()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select ten_mon_hoc from MonHoc",
+                conn);
+            DataTable dtSubject = new DataTable();
+
+            conn.Open();
+
+            da.Fill(dtSubject);
+
+            conn.Close();
+
+            return dtSubject;
+        }
+    }
+}
---
+++ WindowsFormsApp1/QuestionBankForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuestionBank_BUS;
+using QuestionBank_DTO;
+
+namespace QuestionBank_GUI
+{
+    public partial class QuestionBankForm : Form
+    {
+        Subject_BUS subject = new Subject_BUS();
+        public QuestionBankForm()
+        {
+            InitializeComponent();
+
+            DataRow[] dr = subject.getSubjects().Select();
+            utils.LoadSubjects(cbSubject, dr, "ten_mon_hoc");
+            cbSubject.SelectedIndex = 0;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
