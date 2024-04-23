4455fcfde2130303dab697821126b83ffddd2c38
Pham-Tuan-Anh <2051052004anh@ou.edu.vn>
2024-04-23 09:14:18 +0700

    Bo sung

---
+++ OnlineTestingApp_BUS/HomePage_Student_BUS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuestionBank_DAO;
+using QuestionBank_DTO;
+using System.Data;
+
+namespace QuestionBank_BUS
+{
+    public class HomePage_Student_BUS
+    {
+        private HomePage_Student_DAO homepageStudent_Dao;
+
+        public HomePage_Student_BUS()
+        {
+            homepageStudent_Dao = new HomePage_Student_DAO();
+        }
+        public DataTable getLichThi()
+        {
+            return homepageStudent_Dao.getLichThi();
+        }
+    }
+}
